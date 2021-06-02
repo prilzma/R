14d22a5820fe6b769d4edd9527fb4cbfeae0d4f0
prilzma <prilzma@icloud.com>
2021-06-01 22:15:50 -0400

    modification 001
    modification 001

---
+++ scripts/script-001.r
@@ -0,0 +1,45 @@
+library(readr)
+library(stringr)
+library(base)
+library(readxl)
+
+directories=Sys.glob('/path/to/multiple/numerical50/tab/delimited/data/[0-9]*/txt/tab.txt')
+columns=c("data","variables","n","n1","n2","n3","n4","n5","n6","n7")
+
+excel=read_excel('/path/to/data/excel.xlsx') 
+# n where n is a numerical
+remove=excel[n]
+
+numextract <- function(string){ 
+str_extract(string, "\\-*\\d+\\.*\\d*")
+} 
+
+Numextract <- function(string){
+  unlist(regmatches(string,gregexpr("[[:digit:]]+\\.*[[:digit:]]*",string)))
+}
+
+for (i in 1:length(directories)) {
+	data=read.table(directories[i],sep='\t',header=TRUE)
+	data=data[,columns]
+	data=data[grep(paste(remove,collapse="|"),data$n),]
+
+
+	colnames(data)[which(colnames(data) %in% c("data","variables","n"))] <- c(paste("data",Numextract(directories[i])[3],sep='.'),paste("variables",Numextract(directories[i])[3],sep='.'),paste("n",Numextract(directories[i])[3],sep='.'))
+	for (ii in 4:10){
+		colnames(data)[ii] <- paste(grep('n*',colnames(data)[ii],perl=TRUE,value=TRUE),Numextract(directories[i])[3],sep='.')
+
+	}
+
+	write.csv(data,paste(Numextract(directories[i])[3],'data','csv',sep='.'),row.names=FALSE)
+}
+
+
+
+
+
+
+
+
+
+
+
